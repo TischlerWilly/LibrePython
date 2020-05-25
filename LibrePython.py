01a70d77347bd207ecc4acc0de6bbb284753474c
TischlerWilly <oliver.schuft@gmx.de>
2020-05-25 10:28:43 +0200

    Beschriftung Tabkopf frank_

--- LibrePython.py
+++ LibrePython.py
@@ -260,7 +260,7 @@ class slist:
         self.t.set_zelltext_s("F1", "Material")
         self.t.set_zelltext_s("G1", "Kante links     (vo)") 
         self.t.set_zelltext_s("H1", "KaDi")
-        self.t.set_zelltext_s("I1", "Kante rechts   (hi)")
+        self.t.set_zelltext_s("I1", "Kante rechts   (hi)") 
         self.t.set_zelltext_s("J1", "KaDi")
         self.t.set_zelltext_s("K1", "Kante oben     (li)")
         self.t.set_zelltext_s("L1", "KaDi")
